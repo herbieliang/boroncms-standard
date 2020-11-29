0d031ef6675d06efb6415d2f742659fa89ae14b5
Herbie Liang <zachlhb@gmail.com>
2020-11-29 16:29:45 +0800

    格式化代码

--- resources/lang/zh_CN/slide-category.php
+++ resources/lang/zh_CN/slide-category.php
@@ -1,13 +1,14 @@
 <?php
+
 return [
     'labels' => [
-        'SlideCategory' => '幻灯分类',
-        'slide-category' => '幻灯分类'
+        'SlideCategory'  => '幻灯分类',
+        'slide-category' => '幻灯分类',
     ],
     'fields' => [
-        'object_id' => '对象编号',
-        'title' => '分组名称',
-        'description' => '分组说明'
+        'object_id'   => '对象编号',
+        'title'       => '分组名称',
+        'description' => '分组说明',
     ],
-    'options' => []
+    'options' => [],
 ];
